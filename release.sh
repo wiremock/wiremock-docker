8b2a41dbb105a5e1f755bf167ac966edd9f72b0b
rodolpheche <rodolphe.chaigneau@gmail.com>
2016-02-21 02:01:09 +0100

    update release script

--- release.sh
+++ release.sh
@@ -6,10 +6,10 @@ MAJOR_VERSION=$(echo $VERSION | cut -f1 -d".")
 # change wiremock version
 if [[ $MAJOR_LATEST_VERSION == $MAJOR_VERSION ]]
 then
-  sed "s/$VERSION/$LATEST_VERSION/g" readme.md
+  sed -i "s/$VERSION/$LATEST_VERSION/g" readme.md
 else
   # todo
-  sed "s/$VERSION/$LATEST_VERSION/g" readme.md
+  sed -i "s/$VERSION/$LATEST_VERSION/g" readme.md
 fi
 sed -i "s/WIREMOCK_VERSION\(.*\)/WIREMOCK_VERSION $WIREMOCK_VERSION/" Dockerfile
 sed -i "s/^VERSION=.*/VERSION=$LATEST_VERSION/" version.properties

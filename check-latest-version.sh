b40f67fa4b7a1acbb7fbdf05a8ed3d16f8d9a7b1
rodolpheche <rodolphe.chaigneau@gmail.com>
2016-02-21 01:48:52 +0100

    update check-latest-version script

--- check-latest-version.sh
+++ check-latest-version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo -n "WIREMOCK_VERSION=" > version.properties
-curl -s http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/maven-metadata.xml | grep '<latest' | cut -f2 -d">"|cut -f1 -d"<" >> version.properties
+LATEST_VERSION=$(curl -s http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/maven-metadata.xml | grep '<latest' | cut -f2 -d">" | cut -f1 -d"<")
+
+sed -i "s/LATEST_VERSION=\(.*\)/LATEST_VERSION=$LATEST_VERSION/" version.properties

95e2e26521a3d20681c1a8c90992289d56581c8d
rodolpheche <rodolphe.chaigneau@gmail.com>
2016-02-21 02:17:06 +0100

    update release script

--- release.sh
+++ release.sh
@@ -16,5 +16,7 @@ sed -i "s/^VERSION=.*/VERSION=$LATEST_VERSION/" version.properties
 
 
 # commit change
+git config --global user.name "rodolpheche"
+git config --global user.email rodolphe.chaigneau@gmail.com
 git add Dockerfile readme.md version.properties
 git commit -m "upgrade version to $LATEST_VERSION"

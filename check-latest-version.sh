946932441cc14670031089c763fe4e347eb1eae6
rodolpheche <rodolphe.chaigneau@gmail.com>
2016-02-28 23:16:38 +0100

    update release script

--- check-latest-version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-LATEST_VERSION=$(curl -s http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/maven-metadata.xml | grep '<latest' | cut -f2 -d">" | cut -f1 -d"<")
-
-sed -i "s/LATEST_VERSION=\(.*\)/LATEST_VERSION=$LATEST_VERSION/" version.properties
--- release.sh
+++ release.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-MAJOR_LATEST_VERSION=$(echo $LATEST_VERSION | cut -f1 -d".")
+if [ ! $VERSION ]
+then
+  VERSION=$(curl -s http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/maven-metadata.xml | grep '<latest' | cut -f2 -d">" | cut -f1 -d"<")
+fi
+PREVIOUS_VERSION=$(cat Dockerfile | grep "ENV WIREMOCK_VERSION" | cut -f3 -d" ")
+
+
 MAJOR_VERSION=$(echo $VERSION | cut -f1 -d".")
+MAJOR_PREVIOUS_VERSION=$(echo $PREVIOUS_VERSION | cut -f1 -d".")
+
+echo VERSION = $VERSION
+echo PREVIOUS_VERSION = $PREVIOUS_VERSION
+
+echo MAJOR_VERSION = $MAJOR_VERSION
+echo MAJOR_PREVIOUS_VERSION = $MAJOR_PREVIOUS_VERSION
 
 # change wiremock version
-if [[ $MAJOR_LATEST_VERSION == $MAJOR_VERSION ]]
+if [[ $MAJOR_VERSION == $MAJOR_PREVIOUS_VERSION ]]
 then
-  sed -i "s/$VERSION/$LATEST_VERSION/g" readme.md
+  sed -i "s/$PREVIOUS_VERSION/$VERSION/g" readme.md
 else
   # todo
-  sed -i "s/$VERSION/$LATEST_VERSION/g" readme.md
+  sed -i "s/$PREVIOUS_VERSION/$VERSION/g" readme.md
 fi
-sed -i "s/ENV WIREMOCK_VERSION\(.*\)/ENV WIREMOCK_VERSION $LATEST_VERSION/" Dockerfile
-sed -i "s/^VERSION=.*/VERSION=$LATEST_VERSION/" version.properties
-
+sed -i "s/ENV WIREMOCK_VERSION\(.*\)/ENV WIREMOCK_VERSION $VERSION/" Dockerfile
 
 # commit change
-git config --global user.name "rodolpheche"
-git config --global user.email rodolphe.chaigneau@gmail.com
-git add Dockerfile readme.md version.properties
-git commit -m "upgrade version to $LATEST_VERSION"
+git add Dockerfile readme.md
+git commit -m "upgrade version to $VERSION"

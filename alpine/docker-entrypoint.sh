7b27709c63b03b499fa3ed7203cb599e2932389b
rodolpheche <rodolphe.chaigneau@gmail.com>
2019-06-11 22:18:38 +0200

    handle JAVA_OPTS env var even if passing args fix #17

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # Add `java -jar /wiremock-standalone.jar` as command if needed
 if [ "${1#-}" != "$1" ]; then
-	set -- java -cp /var/wiremock/lib/*:/var/wiremock/extensions/* com.github.tomakehurst.wiremock.standalone.WireMockServerRunner "$@"
+	set -- java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* com.github.tomakehurst.wiremock.standalone.WireMockServerRunner "$@"
 fi
 
 # allow the container to be started with `-e uid=`

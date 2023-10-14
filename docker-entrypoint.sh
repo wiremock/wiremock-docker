9ffceebd47976954d3c3ab199bdc04fb37283339
kapishmalik <malikkapish@gmail.com>
2023-10-15 01:06:06 +0530

    feature- add support for wiremock options as env variable - Resolves #78

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,14 @@ set -e
 
 # Set `java` command if needed
 if [ "$1" = "" -o "${1:0:1}" = "-" ]; then
-  set -- java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* wiremock.Run "$@"
+  set -- java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* wiremock.Run "$@"  $WIREMOCK_OPTIONS
 fi
 
 # allow the container to be started with `-e uid=`
 if [ "$uid" != "" ]; then
   # Change the ownership of /home/wiremock to $uid
   chown -R $uid:$uid /home/wiremock
-
-  set -- gosu $uid:$uid "$@"
+  set -- gosu $uid:$uid "$@" $WIREMOCK_OPTIONS
 fi
 
-exec "$@"
+exec "$@" $WIREMOCK_OPTIONS

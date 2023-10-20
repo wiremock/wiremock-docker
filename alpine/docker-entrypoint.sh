6934b45f82f9f90be61c5a55d08d66cd1d027ade
jerosa <espinosajesus8@gmail.com>
2023-10-20 12:50:28 +0200

    Fix WIREMOCK_OPTIONS duplicated

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # Set `java` command if needed
 if [ "$1" = "" -o "${1#-}" != "$1" ]; then
-  set -- java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* wiremock.Run "$@" $WIREMOCK_OPTIONS
+  set -- java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* wiremock.Run "$@"
 fi
 
 # allow the container to be started with `-e uid=`
@@ -12,7 +12,7 @@ if [ "$uid" != "" ]; then
   # Change the ownership of /home/wiremock to $uid
   chown -R $uid:$uid /home/wiremock
 
-  set -- su-exec $uid:$uid "$@"  $WIREMOCK_OPTIONS
+  set -- su-exec $uid:$uid "$@"
 fi
 
 exec "$@"  $WIREMOCK_OPTIONS
